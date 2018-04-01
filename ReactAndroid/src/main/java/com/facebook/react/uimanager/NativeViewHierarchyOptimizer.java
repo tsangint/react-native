3b670c097a5122ce7cb1d5b2d5c8a589962fe90b
luckyzeng <luckyzeng@tencent.com>
2018-04-01 21:01:54 +0800

    [android] fix bug: overflow:hidden does not work

--- ReactAndroid/src/main/java/com/facebook/react/uimanager/NativeViewHierarchyOptimizer.java
+++ ReactAndroid/src/main/java/com/facebook/react/uimanager/NativeViewHierarchyOptimizer.java
@@ -11,6 +11,8 @@ import android.util.SparseBooleanArray;
 import com.facebook.infer.annotation.Assertions;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMapKeySetIterator;
+import com.facebook.yoga.YogaOverflow;
+
 import javax.annotation.Nullable;
 
 /**
@@ -437,6 +439,10 @@ public class NativeViewHierarchyOptimizer {
     }
 
     if (props.hasKey(ViewProps.COLLAPSABLE) && !props.getBoolean(ViewProps.COLLAPSABLE, true)) {
+      return false;
+    }
+
+    if(props.hasKey(ViewProps.OVERFLOW) && props.getString(ViewProps.OVERFLOW).toUpperCase().equals(YogaOverflow.HIDDEN.name())){
       return false;
     }
 
